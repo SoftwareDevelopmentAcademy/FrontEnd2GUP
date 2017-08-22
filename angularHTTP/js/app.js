3175fbd7de67a956f54731fcf72b9b76a6e5d049
Michał Zacharzewski <zahar1982@gmail.com>
2017-08-22 11:11:47 +0200

    user page in angular

--- angularHTTP/js/app.js
+++ angularHTTP/js/app.js
@@ -1,10 +1,19 @@
 var app = angular.module('webapp', ['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    $locationProvider.hashPrefix('');
     $routeProvider
     .when('/', {
        templateUrl: './views/main.html',
         controller: 'first'
+    })
+    .when('/user/:id', {
+        templateUrl: './views/user.html',
+        controller: 'userController'
     });
 });
 
@@ -17,6 +26,17 @@ app.controller('first', function($scope, $http) {
         url: "http://localhost:3000/users"
     }).then(function(success) {
         $scope.users = success.data;
+    }, function(error) {
+        console.error(error);
+    });
+});
+
+app.controller('userController', function($scope, $http, $routeParams) {
+    $http({
+        url: 'http://localhost:3000/users/' + $routeParams.id,
+        method: 'GET'
+    }).then(function(success) {
+        $scope.user = success.data;
     }, function(error) {
         console.error(error);
     });
