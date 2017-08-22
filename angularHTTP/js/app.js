b22dae5de7bbce29b9016f8d14175120eae47c5a
Michał Zacharzewski <zahar1982@gmail.com>
2017-08-22 13:54:00 +0200

    adding users

--- angularHTTP/js/app.js
+++ angularHTTP/js/app.js
@@ -13,7 +13,15 @@ app.config(function($routeProvider) {
     .when('/user/:id', {
         templateUrl: './views/user.html',
         controller: 'userController'
+    })
+    .when('/animals', {
+        templateUrl: './views/animals.html',
+        controller: 'animalsController'
     });
+});
+
+app.controller('main', function($scope) {
+    
 });
 
 app.controller('first', function($scope, $http) {
@@ -28,6 +36,34 @@ app.controller('first', function($scope, $http) {
     }, function(error) {
         console.error(error);
     });
+    
+    $scope.add = function() {
+        $http({
+            url: 'http://localhost:3000/users',
+            method: "POST",
+            data: {
+                name: $scope.name,
+                lastname: $scope.lastname
+            }
+        }).then(function(success) {
+            $scope.users.push(success.data);
+            $scope.name = ''; 
+            $scope.lastname = '';
+        }, function(error) {
+            console.error(error);
+        });
+    }
+    
+    $scope.delete = function(id) {
+        $http({
+            url: 'http://localhost:3000/users/' + id,
+            method: 'DELETE'
+        }).then(function(success) {
+            
+        }, function(error) {
+            console.log(error);
+        });
+    }
 });
 
 app.controller('userController', function($scope, $http, $routeParams) {
@@ -38,5 +74,15 @@ app.controller('userController', function($scope, $http, $routeParams) {
         $scope.user = success.data;
     }, function(error) {
         console.error(error);
+    });
+});
+
+app.controller('animalsController', function($scope, $http) {
+    $http({
+        url: 'http://localhost:3000/animals'
+    }).then(function(success) {
+        $scope.animals = success.data;
+    }, function(error) {
+        
     });
 });
