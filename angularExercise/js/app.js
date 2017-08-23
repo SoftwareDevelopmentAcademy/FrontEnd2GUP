afc2ed06e6297ab2b9ab1673e10de934eb2809a6
pawel.walnicki@gmail.com <zahar1982@gmail.com>
2017-08-23 10:42:53 +0200

    angular data edit examples

--- angularExercise/js/app.js
+++ angularExercise/js/app.js
@@ -2,6 +2,8 @@ var app = angular.module('webapp', ['ngRoute']);
 var baseAJAXUrl = 'http://localhost:3000/';
 
 app.controller('mainController', function($scope, $http) {
+    // kod, ktory sie wykona od razu po uruchomieniu kontrolera
+    
     $http({
         url: baseAJAXUrl + 'subject',
         method: 'GET'
@@ -10,4 +12,31 @@ app.controller('mainController', function($scope, $http) {
     }, function(error) {
         console.error(error);
     });
+    
+    $scope.actualEditId = null;
+    // kod, ktory sie wykona po wywolaniu funkcji edit
+    $scope.edit = function(subject) {
+        if($scope.actualEditId != subject.id) {
+            $scope.actualEditId = subject.id;
+        } else {
+            //alert("Ten element już jest edytowany!");
+            $http({
+                url: baseAJAXUrl + 'subject/' + subject.id,
+                method: 'PUT',
+                data: subject
+            }).then(function(success) {
+                console.log(success.data);
+                $scope.actualEditId = null;
+            }, function(error) {
+                console.error(success.error);
+            });
+        }
+    }
+    
+    $scope.tryEdit = function(keyCode, subject) {
+        if(keyCode === 13) {
+            $scope.edit(subject);
+        }
+    }
+    
 });
