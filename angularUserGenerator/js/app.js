2944d131bb3fa08b391a08646229b52468d28330
pawel.walnicki@gmail.com <zahar1982@gmail.com>
2017-08-23 15:01:23 +0200

    synchro in ajax data

--- angularUserGenerator/js/app.js
+++ angularUserGenerator/js/app.js
@@ -1,20 +1,35 @@
 var app = angular.module('webapp', []);
 
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $http) {
     $scope.makeUser = function(name, lastname) {
         return { name: name, lastname: lastname }
     }
 /*
 1. Pobierzmy wszystkie zasoby z bazy danych - /db
     - zasoby, ktore zostaną zwrócone, przypiszmy do $scope.resources
-2. Przy generowaniu losowego użytkownika  (w forze wypełniającym $scope.users) wylosujmy dane (imię i nazwisko).
-    - najpierw ustalmy płeć użytkownika, którego chcemy wypełnić
-        > losujemy liczbe <0, 1>
-        > następnie przypisujemy do zmiennej var sex; wartość 'male' w przypadku 0, 'female' w przypadku 1
-        > gdy chciałbym wylosować imiona i nazwiska żeńskie:
-            - sex + '_name', sex + '_lastname'
-
 */
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3000/db'
+    }).then(function(success) {
+        $scope.resources = success.data;
+        $scope.loadData();
+    }, function(error) {
+        
+    }); 
+    
+    console.log($scope.resources);
+    
+    $scope.loadData = function(sex) {
+        if(['male', 'female'].indexOf(sex.toLowerCase()) == -1) {
+            // losujemy liczbe <0, 1>
+            var r = Math.round(Math.random()); // <0, 1>
+            // następnie przypisujemy do zmiennej var sex; wartość 'male' w przypadku 0, 'female' w przypadku 1
+            sex = (r === 0) ? 'male' : 'female';
+        }
+        // teraz mamy poprawnie wylosowana lub przekazana plec
+        // 
+    }
     
     
     $scope.users = [];
