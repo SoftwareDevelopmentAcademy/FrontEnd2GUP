5949dbf0ca33b65418e82bef763f0fa9cbb75a20
pawel.walnicki@gmail.com <zahar1982@gmail.com>
2017-08-25 10:04:57 +0200

    pizzer exercise 4

--- pizzer/js/app.js
+++ pizzer/js/app.js
@@ -14,4 +14,28 @@ $(document).ready(function() {
         $loginLink.removeClass('d-none');
         $loginBox.addClass('d-none');
     });
+    
+    var pizza = [
+        { id: 1, name: 'Margarita', points: 1, image: 'margarita.png', costs: 0 }
+    ];
+    
+    $('<img/>', { class: 'pizza' })
+        .attr('src', './images/margarita.png')
+        .appendTo('.pizza-container');
+    
+    var $pizza = $('.pizza');
+    
+    $pizza.on('mouseover', function(e) {
+        $pizza.attr('height', 0.9 * $pizza.height());
+        
+    });
+        
+    $pizza.on('mouseleave', function(e) {
+        $pizza.attr('height', 1.11 * $pizza.height()); 
+    });
+    
+    $pizza.on('click', function(e) {
+        alert('Kliknięto na pizze!');
+    });
+    
 });
