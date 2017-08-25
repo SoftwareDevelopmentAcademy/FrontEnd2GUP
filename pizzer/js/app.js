b81eff903358379aef2ab2b1a20f16b6078164a9
pawel.walnicki@gmail.com <zahar1982@gmail.com>
2017-08-25 08:52:27 +0200

    pizzer ex 1

---
+++ pizzer/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var $loginLink = $('.loginLink');
+    var $loginBox = $('.loginBox');
+    var $handleLoginBtn = $('.handleLogin');
+    
+    $loginLink.on('click', function(e) {
+        e.preventDefault();
+        $loginLink.addClass('d-none');
+        $loginBox.removeClass('d-none');
+    });
+    
+    $handleLoginBtn.on('click', function(e) {
+        alert('Zalogowano');
+        $loginLink.removeClass('d-none');
+        $loginBox.addClass('d-none');
+    });
+});
